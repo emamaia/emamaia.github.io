1126442a047a50e8b74152240b1d8163c2cd5065
Emanuelle Maia <ema_maia@hotmail.com>
2019-10-15 15:27:53 -0300

    Adiciona exercícios jQuery

---
+++ jQuery-exercicios/Exercícios II/site/js/page1.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $('#btnAnimate').click(function(){
+        $('.alert').animate({
+            height: '150px',
+            width: '500px'
+        })   
+
+})
+
+    $("#btnReset").click(function(){
+        $(".alert").animate({
+            height: '50px',
+            width: '300px'
+        })
+    })
+
+})
---
+++ jQuery-exercicios/Exercícios II/site/js/page4.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.button').click(function(){
+        $('.card1').slideDown(2000);
+        $('.card2').slideDown(4000);
+        $('.card3').slideDown(6000);
+    })
+})
